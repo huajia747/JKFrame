d3161e5946e55e0e27b87cfc5574746d758c6502
huajia747 <54095381+huajia747@users.noreply.github.com>
2023-08-14 23:57:38 +0800

    换成更严谨的Mono单例基类
    自动检测、多余的物体自动销毁，保证场景中只有一个单例物体

--- JKFrame/Scripts/1.Base/Singleton/SingletonMono.cs
+++ JKFrame/Scripts/1.Base/Singleton/SingletonMono.cs
@@ -4,13 +4,56 @@ namespace JKFrame
 {
     public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
     {
-        public static T Instance;
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<T>();
+                    if (FindObjectsByType<T>(FindObjectsSortMode.None).Length > 1)
+                    {
+                        Debug.LogWarning("单例物体重复存在：<color=#6cd6cd>" + _instance.name);
+                        return _instance;
+                    }
+
+                    if (_instance == null)
+                    {
+                        var instanceName = typeof(T).Name;
+                        var instanceObj = GameObject.Find(instanceName);
+                        if (!instanceObj)
+                        {
+                            instanceObj = new GameObject(instanceName);
+                            instanceObj.AddComponent<T>();
+                            // DontDestroyOnLoad(instanceObj);
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public static bool IsCreated()
+        {
+            return _instance is not null;
+        }
+
+        public static bool NotCreated()
+        {
+            return _instance == null;
+        }
+
         protected virtual void Awake()
         {
-            if (Instance == null)
+            //在切换场景、加载场景时，当新场景中检测到之前存在同类单例时，把自己销毁
+            if (_instance != null && _instance != this)
             {
-                Instance = this as T;
+                Destroy(gameObject);
+                return;
             }
+            _instance = this as T;
         }
     }
 }
