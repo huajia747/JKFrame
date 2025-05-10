6d5b0206e78d5bca0c49f742cd245a6fc7d31b35
实习生小黄 <474040353@qq.com>
2025-05-10 17:25:42 +0800

    统一命名空间

--- JKFrame/Scripts/7.MyTool/FxManager.cs
+++ JKFrame/Scripts/7.MyTool/FxManager.cs
@@ -1,99 +1,107 @@
 using System.Collections.Generic;
-using JKFrame;
-using JKFrame.MyTool;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class FxManager : SingletonMono<FxManager>
+namespace JKFrame.MyTool
 {
-    private void InternalPlayFx(GameObject fxObj, bool autoPoolBack)
+    public class FxManager : SingletonMono<FxManager>
     {
-        var playableTime = -1.0f;
-        // 如果有timeline要播放
-        if (fxObj.TryGetComponent(out PlayableDirector p))
+        private void InternalPlayFx(GameObject fxObj, bool autoPoolBack)
         {
-            p.Play();
-            playableTime = (float)p.duration;
-        }
-        if (autoPoolBack && !fxObj.TryGetComponent(out PushPoolInTime pushPool))
-        {
-            var newPushPool = fxObj.AddComponent<PushPoolInTime>();
-            if (playableTime > 0)
+            var playableTime = -1.0f;
+            // 如果有timeline要播放
+            if (fxObj.TryGetComponent(out PlayableDirector p))
             {
-                // 如果有Timeline的话，以Timeline时长为准，因为会隐藏某些子节点，导致计算粒子时长不对。
-                newPushPool.lifetime = playableTime;
-                newPushPool.autoCalculate = false;
-            }
-            else
-            {
-                newPushPool.autoCalculate = true;
-            }
-        }
-    }
-    
-    public GameObject PlayFx(GameObject fxPrefab, Transform parentTransform, bool autoPoolBack, string idPrefix = "")
-    {
-        var fxObj = SimplePool.GetGameObject(fxPrefab, parentTransform, idPrefix);
-
-        InternalPlayFx(fxObj, autoPoolBack);
-
-        return fxObj;
-    }
-    public GameObject PlayFx(GameObject fxPrefab, Vector3 position, Quaternion rotation, bool autoPoolBack, string idPrefix = "")
-    {
-        var fxObj = SimplePool.GetGameObject(fxPrefab, position, rotation, idPrefix);
-        
-        InternalPlayFx(fxObj, autoPoolBack);
-
-        return fxObj;
-    }
-
-    private Dictionary<string, GameObject> _singletonFxRecord = new Dictionary<string, GameObject>(); // 所有单例的特效，都要记录下来，下次播放单例特效时，先检测所有记录中是否有正在播放的。
-    
-    public GameObject PlaySingletonFx(GameObject fxPrefab, Vector3 position, Quaternion rotation, bool autoPoolBack, string idPrefix = "")
-    {
-        // 先从所有记录的单例特效中，检测是否有同一个特效
-        var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
-        StopSingletonFx(fxKeyName);
-
-        var fxGo = PlayFx(fxPrefab, position, rotation, autoPoolBack, idPrefix);
-        
-        // 新播放的特效记录进单例记录中
-        _singletonFxRecord.Add(fxKeyName, fxGo);
-
-        return fxGo;
-    }
-    
-    public GameObject PlaySingletonFx(GameObject fxPrefab, Transform parentTransform, bool autoPoolBack, string idPrefix = "")
-    {
-        // 先从所有记录的单例特效中，检测是否有同一个特效
-        var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
-        StopSingletonFx(fxKeyName);
-
-        var fxGo = PlayFx(fxPrefab, parentTransform, autoPoolBack, idPrefix);
-        
-        // 新播放的特效记录进单例记录中
-        _singletonFxRecord.Add(fxKeyName, fxGo);
-
-        return fxGo;
-    }
-
-    private void StopSingletonFx(string fxKeyName)
-    {
-        if (_singletonFxRecord.ContainsKey(fxKeyName)) // 记录中已经有此单例特效
-        {
-            if (_singletonFxRecord[fxKeyName].activeSelf) // 次单例特效激活着，没被回收
-            {
-                _singletonFxRecord[fxKeyName].GameObjectPushPool(); // 回收它
+                p.Play();
+                playableTime = (float)p.duration;
             }
 
-            _singletonFxRecord.Remove(fxKeyName); // 清楚记录，为之后播新的，加入新的做准备。
+            if (autoPoolBack && !fxObj.TryGetComponent(out PushPoolInTime pushPool))
+            {
+                var newPushPool = fxObj.AddComponent<PushPoolInTime>();
+                if (playableTime > 0)
+                {
+                    // 如果有Timeline的话，以Timeline时长为准，因为会隐藏某些子节点，导致计算粒子时长不对。
+                    newPushPool.lifetime = playableTime;
+                    newPushPool.autoCalculate = false;
+                }
+                else
+                {
+                    newPushPool.autoCalculate = true;
+                }
+            }
         }
-    }
 
-    public void StopSingletonFx(GameObject fxPrefab, string idPrefix = "")
-    {
-        var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
-        StopSingletonFx(fxKeyName);
+        public GameObject PlayFx(GameObject fxPrefab, Transform parentTransform, bool autoPoolBack,
+            string idPrefix = "")
+        {
+            var fxObj = SimplePool.GetGameObject(fxPrefab, parentTransform, idPrefix);
+
+            InternalPlayFx(fxObj, autoPoolBack);
+
+            return fxObj;
+        }
+
+        public GameObject PlayFx(GameObject fxPrefab, Vector3 position, Quaternion rotation, bool autoPoolBack,
+            string idPrefix = "")
+        {
+            var fxObj = SimplePool.GetGameObject(fxPrefab, position, rotation, idPrefix);
+
+            InternalPlayFx(fxObj, autoPoolBack);
+
+            return fxObj;
+        }
+
+        private Dictionary<string, GameObject>
+            _singletonFxRecord = new Dictionary<string, GameObject>(); // 所有单例的特效，都要记录下来，下次播放单例特效时，先检测所有记录中是否有正在播放的。
+
+        public GameObject PlaySingletonFx(GameObject fxPrefab, Vector3 position, Quaternion rotation, bool autoPoolBack,
+            string idPrefix = "")
+        {
+            // 先从所有记录的单例特效中，检测是否有同一个特效
+            var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
+            StopSingletonFx(fxKeyName);
+
+            var fxGo = PlayFx(fxPrefab, position, rotation, autoPoolBack, idPrefix);
+
+            // 新播放的特效记录进单例记录中
+            _singletonFxRecord.Add(fxKeyName, fxGo);
+
+            return fxGo;
+        }
+
+        public GameObject PlaySingletonFx(GameObject fxPrefab, Transform parentTransform, bool autoPoolBack,
+            string idPrefix = "")
+        {
+            // 先从所有记录的单例特效中，检测是否有同一个特效
+            var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
+            StopSingletonFx(fxKeyName);
+
+            var fxGo = PlayFx(fxPrefab, parentTransform, autoPoolBack, idPrefix);
+
+            // 新播放的特效记录进单例记录中
+            _singletonFxRecord.Add(fxKeyName, fxGo);
+
+            return fxGo;
+        }
+
+        private void StopSingletonFx(string fxKeyName)
+        {
+            if (_singletonFxRecord.ContainsKey(fxKeyName)) // 记录中已经有此单例特效
+            {
+                if (_singletonFxRecord[fxKeyName].activeSelf) // 次单例特效激活着，没被回收
+                {
+                    _singletonFxRecord[fxKeyName].GameObjectPushPool(); // 回收它
+                }
+
+                _singletonFxRecord.Remove(fxKeyName); // 清楚记录，为之后播新的，加入新的做准备。
+            }
+        }
+
+        public void StopSingletonFx(GameObject fxPrefab, string idPrefix = "")
+        {
+            var fxKeyName = SimplePool.GetKeyName(fxPrefab, idPrefix);
+            StopSingletonFx(fxKeyName);
+        }
     }
 }
